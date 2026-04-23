1c959c8e10f7b539851049cd173560d96ceb61fc
harshit <varma789ajay@gmail.com>
2026-04-23 13:17:23 +0530

    database creation done using python file handling in json structure

---
+++ app/database.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+FILE = "students.json"
+
+def load_students():
+    if not os.path.exists(FILE):
+        return []
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def save_students(data):
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
